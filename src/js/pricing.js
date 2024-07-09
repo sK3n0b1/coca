8360cc08525465ff0423329f23f74d9523f62539
Vsevolod Alexeev <holodilnikzenit111117@gmail.com>
2024-07-09 12:14:56 +0300

    Created pricing:hero section

---
+++ src/js/pricing.js
@@ -0,0 +1,11 @@
+import '../assets/scss/pricing.scss'
+
+//components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
+
+
+useTheme();
+useBurger();
+usePriceSwitcher();
