5b8ac3e5656ed15e41a8236d3338eadfb1609961
Vsevolod Alexeev <holodilnikzenit111117@gmail.com>
2024-07-10 15:41:35 +0300

    Created contact:get-started section

---
+++ src/js/components/contact/phone.js
@@ -0,0 +1,18 @@
+import intlTelInput from 'intl-tel-input';
+import 'intl-tel-input/build/css/intlTelInput.css';
+
+export const usePhone = () => {
+  const input = document.querySelector('#phone');
+
+  intlTelInput(input, {
+    initialCountry: 'auto',
+    geoIpLookup: (callback) => {
+      fetch('https://ipapi.co/json')
+        .then((res) => res.json())
+        .then((data) => callback(data.country_code))
+        .catch(() => callback('us'));
+    },
+    utilsScript:
+      'https://cdn.jsdelivr.net/npm/intl-tel-input@23.2.1/build/js/utils.js',
+  });
+};
--- src/js/contact.js
+++ src/js/contact.js
@@ -3,8 +3,10 @@ import '../assets/scss/contact.scss'
 //components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
 
 
 
 useTheme();
 useBurger();
+usePhone();
