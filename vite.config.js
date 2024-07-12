161ef2ddd12effdbb2c9a92fbb5746dbb6b59279
Vsevolod Alexeev <holodilnikzenit111117@gmail.com>
2024-07-12 17:11:21 +0300

    Fixed link

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         main: './src/index.html',
         pricing: './src/pricing.html',
         contact: './src/contact.html',
+        blog: './src/blog.html',
         detail: './src/detail.html',
       },
     },
